a799f23867fe302d2414b06af593cc95b5fa9fed
Stephen Cagle <samedhi@gmail.com>
2013-03-05 13:50:11 -0800

    A snapshot of <x-accordion> not seeming to accept a <template> value

---
+++ web/accordion_problems.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+import 'package:web_ui/web_ui.dart';
+
+String search_term = "apple";
+List<String> fruits = const [ 'Apple', 'Apricot', 'Avocado',
+                                'Banana', 'Blackberry', 'Blackcurrant', 'Blueberry',
+                                'Currant', 'Cherry', 'Clementine', 'Date', 'Durian',
+                                'Fig', 'Gooseberry', 'Grape', 'Grapefruit', 'Guava',
+                                'Huckleberry', 'Kiwi', 'Lemon', 'Lime', 'Lychee',
+                                'Mandarine', 'Mango', 'Cantaloupe', 'Honeydew melon',
+                                'Nectarine', 'Orange', 'Peach', 'Pear', 'Plum',
+                                'Pineapple', 'Pomegranate', 'Raspberry', 'Redcurrant',
+                                'Star fruit', 'Strawberry', 'Tangerine', 'Tomato',
+                                'Watermelon'];
+
+void main() {
+  //useShadowDom = true; // to enable use of experimental Shadow DOM in the browser  
+}
